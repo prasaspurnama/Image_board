c9f84f439c0adcb7517c52d5b2ef50a559c22e9f
prasaspurnama <andripurnama@outlook.com>
2014-09-25 20:42:18 +0700

    user imgboard php code added

--- imgboard.php
+++ imgboard.php
@@ -1,10 +1,4 @@
 <?
-# Futallaby 040103
-#
-# For setup instructions and latest version, please visit:
-# http://www.1chan.net/futallaby/
-#
-# Based on GazouBBS and Futaba
 
 include "config.php";
 include "strings_e.php";		//String resource file
